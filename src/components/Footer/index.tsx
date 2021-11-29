08b1b7fd862c40f9d62797d659238a1c8df089f1
ViniciusTei <viniciustprates@gmail.com>
2021-11-29 11:32:19 -0300

    adicionado estatuto

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 
 import styles from './styles.module.css';
 import { TiSocialInstagram } from 'react-icons/ti'
+import { GiBatMask } from 'react-icons/gi'
 
 export function Footer() {
   
@@ -18,13 +19,25 @@ export function Footer() {
         </span>
       </a>
 
-      <a 
-        href="https://www.instagram.com/abat_caverna/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <TiSocialInstagram color="eaeaea" width={32} height={32}/>
-      </a>
+
+      <div className={styles.social}>
+        <a 
+          href="https://rain-diver-f37.notion.site/Estatuto-Abat-Caverna-e04f83cf85cb43a7ac30668e64f3fe33"
+          target="_blank"
+          rel="noopener noreferrer"
+          title="Estatuto"
+        >
+          <GiBatMask color="eaeaea" width={32} height={32}/>
+        </a>
+        <a 
+          href="https://www.instagram.com/abat_caverna/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <TiSocialInstagram color="eaeaea" width={32} height={32}/>
+        </a>
+      </div>
+      
     </footer>
   )
 }
