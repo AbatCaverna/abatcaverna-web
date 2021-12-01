c3d5ea066df0914d2758a076429f70c84551b742
ViniciusTei <viniciustprates@gmail.com>
2021-12-01 20:18:50 -0300

    add blur image to lazy load

--- src/components/Historia/index.tsx
+++ src/components/Historia/index.tsx
@@ -1,7 +1,15 @@
 import Image from 'next/image'
+import { useRgbDataURL } from '../../hooks/useBlur';
 import styles from './styles.module.css'
 
 export function Historia() {
+  
+  function getBlurImg() {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const img = useRgbDataURL(0, 0, 0)
+    return img
+  }
+
   return (
     <section id="historia" className={styles.historia}>
       <h2>História</h2>
@@ -11,7 +19,14 @@ export function Historia() {
           Somos uma república de Florestal MG, idealizada em 2016 e fundada em 2017. 
           Fomos criados não apenas para ser uma república mas uma família, amigos e irmãos.
         </p>
-        <Image src="/casa/casa-nova.jpeg" alt="Foto da casa" width={400} height={500} />
+        <Image 
+          src="/casa/casa-nova.jpeg" 
+          alt="Foto da casa"
+          placeholder="blur"
+          blurDataURL={getBlurImg()}
+          width={400} 
+          height={500} 
+        />
       </div>
     </section>
   )
