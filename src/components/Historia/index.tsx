3470d26d216a1e8eb5d0434d78904b258825a3f4
ViniciusTei <viniciustprates@gmail.com>
2021-12-03 06:37:28 -0300

    add animation to slide show

--- src/components/Historia/index.tsx
+++ src/components/Historia/index.tsx
@@ -1,14 +1,18 @@
 import Image from 'next/image'
 import { useRgbDataURL } from '../../hooks/useBlur';
+import ImageSlideView from '../ImageSlideView';
 import styles from './styles.module.css'
 
 export function Historia() {
-  
-  function getBlurImg() {
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    const img = useRgbDataURL(0, 0, 0)
-    return img
-  }
+  const images = [
+    '/casa/casa-nova.jpeg',
+    '/historia/conexoes.jpeg',
+    '/casa/1.jpg',
+    '/casa/golo-golo.jpeg',
+    '/casa/trote.jpg',
+    '/casa/galera.jpg',
+    '/casa/festa.jpg'
+  ];
 
   return (
     <section id="historia" className={styles.historia}>
@@ -19,13 +23,8 @@ export function Historia() {
           Somos uma república de Florestal MG, idealizada em 2016 e fundada em 2017. 
           Fomos criados não apenas para ser uma república mas uma família, amigos e irmãos.
         </p>
-        <Image 
-          src="/casa/casa-nova.jpeg" 
-          alt="Foto da casa"
-          placeholder="blur"
-          blurDataURL={getBlurImg()}
-          width={400} 
-          height={500} 
+        <ImageSlideView
+          images={images}
         />
       </div>
     </section>
