f32661b20f961b4cb48f827c5e1602f9ee240e10
ViniciusTei <viniciustprates@gmail.com>
2021-12-08 17:53:58 -0300

    add moradores services

---
+++ src/pages/api/moradores.ts
@@ -0,0 +1,60 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { ObjectId } from 'mongodb';
+import type { NextApiRequest, NextApiResponse } from 'next'
+import ConnectDatabase from '../../services/mongo'
+
+type Morador = {
+  _id: string;
+  nome: string;
+  apelido: string;
+  ano_entrada: number;
+  curso: string;
+  imagem: string;
+  instagram: string;
+  cachaca_para_tomar: number;
+  cachaca_ja_tomada: number;
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const database = (await ConnectDatabase).db('abatcaverna')
+  if (req.method === 'GET') {
+    const moradores = await database
+      .collection("moradores")
+      .find({})
+      .toArray();
+    
+    res.status(200).send(moradores)
+  }
+
+  if (req.method === 'PUT') {
+    const { method_action, morador_id } = req.body
+
+    if (method_action === 'sum_cachaca') {
+      // define uma nova cachaca para um morador
+      await database.collection("moradores").updateOne(
+        {
+            _id: new ObjectId(morador_id),
+        },
+        { $inc: { cachaca_para_tomar: 1 } }
+    );
+
+      res.status(200).send('Cachaca adicionada')
+    } else if (method_action === 'drunk_cachaca') {
+      await database.collection("moradores").updateOne(
+        { _id: new ObjectId(morador_id) },
+        { $inc: { 
+          cachaca_para_tomar: -1,
+          cachaca_ja_tomada: 1
+        }}
+      )
+      res.status(200).send('Cachaca bebida')
+    }
+    
+               
+    
+  }
+  
+}
--- src/pages/cachaca.tsx
+++ src/pages/cachaca.tsx
@@ -1,5 +1,7 @@
+import { GetServerSideProps } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import MoradoresService from '../services/MoradoresService'
 import styles from '../styles/Cachaca.module.css'
 
 export default function Cachaca() {
@@ -16,9 +18,25 @@ export default function Cachaca() {
           <Image src="/images/header-quadro-cachaca.svg" alt="Garrafa 51" width="400px" height="97px"/>
         </header>
         <div>
-          
+
         </div>
       </main>
     </div>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  try {
+    const service = new MoradoresService()
+    const response = await service.getMoradores()
+
+    return {
+      props: { moradores: response.data },
+    }
+  } catch (e) {
+    console.error(e)
+    return {
+      props: { isConnected: false },
+    }
+  }
 }
