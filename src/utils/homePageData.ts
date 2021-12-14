d1740bca7227d7bd7294a71e180e3bf788b6358c
ViniciusTei <viniciustprates@gmail.com>
2021-12-14 08:28:41 -0300

    add blur data to all front page images

---
+++ src/utils/homePageData.ts
@@ -0,0 +1,105 @@
+export const Data = {
+  moradores: [
+    {
+      nome: 'Selina',
+      apelido: 'Selina',
+      dataEntrada: 2019,
+      curso: 'Mascote',
+      image: '/moradores/selina.jpeg',
+      base64: ''
+    },
+    {
+      nome: 'Matheus S',
+      apelido: 'Teta',
+      dataEntrada: 2016,
+      curso: 'Ciência da Computação',
+      image: '/moradores/teta.jpeg',
+      instagram: 'https://www.instagram.com/bu.matheus/',
+      base64: ''
+    },
+    {
+      nome: 'Lucas Takeshi',
+      apelido: 'Coleira',
+      dataEntrada: 2016,
+      curso: 'Ciência da Computação',
+      image: '/moradores/coleira.jpeg',
+      instagram: 'https://www.instagram.com/lucastakeshii/',
+      base64: ''
+    },
+    {
+      nome: 'Victor Hugo',
+      apelido: 'Cocorico',
+      dataEntrada: 2018,
+      curso: 'Ciência da Computação',
+      image: '/moradores/cocorico.jpeg',
+      instagram: 'https://www.instagram.com/victorhugo_99/',
+      base64: ''
+    },
+    {
+      nome: 'Vinicius T',
+      apelido: 'Chapoca',
+      dataEntrada: 2019,
+      curso: 'Ciência da Computação',
+      image: '/moradores/chapoca.jpeg',
+      instagram: 'https://www.instagram.com/vinciust/',
+      base64: ''
+    },
+    {
+      nome: 'Rubens',
+      apelido: 'Alan',
+      dataEntrada: 2021,
+      curso: 'Agronomia',
+      image: '/moradores/alan.jpeg',
+      instagram: 'https://www.instagram.com/rubens5664/',
+      base64: ''
+    },
+    {
+      nome: 'Heron',
+      apelido: '?',
+      dataEntrada: 2021,
+      curso: 'Ciência da Computação',
+      image: '/moradores/heron.jpeg',
+      instagram: 'https://www.instagram.com/heron_f4/',
+      base64: ''
+    },
+    {
+      nome: 'Matheus R',
+      apelido: 'Pão',
+      dataEntrada: 2021,
+      curso: 'Engenharia de Alimentos',
+      image: '/moradores/pao.jpeg',
+      instagram: '',
+      base64: ''
+    }
+  ],
+  historyImages: [
+    {
+      img: '/casa/casa-nova.jpeg',
+      imgBase64: ''
+    },
+    {
+      img: '/historia/conexoes.jpeg',
+      imgBase64: ''
+    },
+    {
+      img: '/casa/1.jpeg',
+      imgBase64: ''
+    },
+    {
+      img: '/casa/golo-golo.jpeg',
+      imgBase64: ''
+    },
+    {
+      img: '/casa/trote.jpeg',
+      imgBase64: ''
+    },
+    {
+      img: '/casa/galera.jpeg',
+      imgBase64: ''
+    },
+    {
+      img: '/casa/festa.jpeg',
+      imgBase64: ''
+    },
+  ],
+}
