1b4d519283aee54fa20139865cba96a13a80d073
ViniciusTei <viniciustprates@gmail.com>
2022-04-05 14:45:04 -0300

    build: fix context environment

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-if [$CONTEXT == "branch-deploy"] ;
-  then export NEXTAUTH_URL=$STAGING_URL
-else if [$CONTEXT == "production"] ; 
-  then export NEXTAUTH_URL=$PRODUCTION_URL
-fi
-# npm is setup to always use $MY_CONTEXT_VAR, and assumes you have it set before you start!
-yarn build
