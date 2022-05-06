6bd272d9e0063d83fc9df5c3ce33f4215d7be732
ViniciusTei <viniciutprates@gmail>
2022-05-05 21:00:30 -0300

    fix: home checkout button

--- src/components/Home/NossaLoja/index.tsx
+++ src/components/Home/NossaLoja/index.tsx
@@ -1,7 +1,11 @@
+import { signIn, useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 import Stripe from "stripe";
+import CheckoutService from "../../../services/CheckoutService";
 import ProdutosService from "../../../services/ProdutosService";
+import getStripe from "../../../services/stripejs";
 import ProductCard from "../../Loja/ProductCard";
+import Loader from "../../Shared/Loading";
 import styles from "./styles.module.css";
 
 type Produtos = {
@@ -11,6 +15,29 @@ type Produtos = {
 
 export default function NossaLoja() {
   const [produtos, setProdutos] = useState([] as Produtos[])
+  const [loading, setLoading] = useState(false)
+  const session = useSession()
+
+  async function handleBuyButton(priceId: string) {
+    try {
+      setLoading(true)
+      const checkoutService = new CheckoutService()
+      if (session.status === "unauthenticated") await signIn("google")
+
+      const email = session.data?.user?.email!
+
+      const response = await checkoutService.createCheckoutSession(priceId, email)
+      const stripe = await getStripe()
+  
+      await stripe?.redirectToCheckout({ sessionId: response.data.sessionId })
+      
+    } catch (error) {
+      console.log('redirect to checkout error',error)
+      alert(`Error! ${error}`)
+    } finally {
+      setLoading(false)
+    }
+  }
 
   function fetchProducts() {
     const produtcService = new ProdutosService()
@@ -30,9 +57,15 @@ export default function NossaLoja() {
           <ProductCard
             key={produto.price.id}
             data={produto}
-            handleClick={(ev) => console.log(ev)}
+            handleClick={handleBuyButton}
             />
         ))}
+        {loading && (
+          <div className={styles.loading}>
+            <Loader/>
+            <p>Carregando...</p>
+          </div>
+        )}
       </div>
     </section>
   ) 
