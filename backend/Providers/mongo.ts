e0512cd545d4de2d760346cdeb4acf5d3cd45b28
@vinicius.teixeira2 <vinicius.teixeira@cbrdoc.com.br>
2022-10-03 17:17:44 -0300

    add database name variable

--- backend/Providers/mongo.ts
+++ backend/Providers/mongo.ts
@@ -1,24 +1,33 @@
-import { MongoClient } from 'mongodb'
+import { Db, MongoClient } from 'mongodb'
 
-export default async function connectMongo() {
-  const uri = process.env.MONGODB_URI
+type Connection = {
+  database: Db | undefined
+}
+
+export default async function connectMongo(): Promise<Connection> {
+  const URI = process.env.MONGODB_URI
+  const DATABASE_NAME = process.env.MONGODB_DB_NAME
   const options = {}
   
-  if (!process.env.MONGODB_URI || !uri) {
+  if (!process.env.MONGODB_URI || !URI) {
     throw new Error('Please add your Mongo URI to .env.local')
+  }
+
+  if (!DATABASE_NAME) {
+    throw new Error('Please add your Mongo Database name to .env.local')
   }
   
   // Export a module-scoped MongoClient promise. By doing this in a
   // separate module, the client can be shared across functions.
-  const client = new MongoClient(uri, options)
+  const client = new MongoClient(URI, options)
   const clientPromise = client.connect()
   try {
-    const database = (await clientPromise).db('abatcaverna')
-    console.info(`LOG[SERVER](${new Date().toDateString()}): Database connected!`)
+    const database = (await clientPromise).db(DATABASE_NAME)
+    console.info(`LOG[SERVER](${new Date().toDateString()}): Connected to database ${DATABASE_NAME}!`)
     return { database }
   } catch (error) {
     console.error(`Error[SERVER](${new Date().toDateString()}): Database connection error!`, error)
-    return { }
+    return { database: undefined }
   }
 
 }
