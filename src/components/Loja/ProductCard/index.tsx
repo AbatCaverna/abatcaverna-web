77521cdd69cecd472a35944c3274b037fd5d19af
@vinicius.teixeira2 <vinicius.teixeira@cbrdoc.com.br>
2022-10-05 18:36:14 -0300

    feat update product card styles

--- src/components/Loja/ProductCard/index.tsx
+++ src/components/Loja/ProductCard/index.tsx
@@ -1,8 +1,11 @@
 import Image from "next/image";
 import Stripe from "stripe";
-import useCarrinho from "../../../hooks/useCarrinho";
 import { BsFillCartPlusFill } from "react-icons/bs";
+
+import useCarrinho from "hooks/useCarrinho";
+
 import styles from "./styles.module.css";
+import { Button, Card } from "components/Shared";
 
 type ProductsResponse = {
   product: Stripe.Response<Stripe.Product>;
@@ -40,30 +43,33 @@ export default function ProductCard({ data, handleClick }: ProductCardProps) {
   }
 
   return (
-    <div className={styles.card}>
-      <strong>{product.name}</strong>
-      <div className={styles.card_image}>
-        <Image
-          src={product.images[0] || noImage}
-          alt="Produto"
-          layout="fill"
-        />
+    <Card style={{
+      maxWidth: '350px',
+      minWidth: '295px',
+    }}>
+      <>
+        <strong className={styles.card_title}>{product.name}</strong>
+        <div className={styles.card_image}>
+          <Image
+            src={product.images[0] || noImage}
+            alt="Produto"
+            layout="fill"
+          />
 
-      </div>
-      <p>{transformPrice(price.unit_amount)} <span>({calculatePriceWIthoutTax(price.unit_amount, product.metadata.taxas)} + {transformPrice(product.metadata.taxas)} taxas)</span></p>
-      <div className={styles.btn_container}>
-        <button
-          type="button"
-          className={styles.btn_buy}
-          onClick={() => addToCart(data)}
-        ><BsFillCartPlusFill color="#000" size="1rem"/></button>
-        <button
-          type="button"
-          className={styles.btn_buy}
-          onClick={() => handleClick(data.price.id)}
-        >Comprar</button>
-      </div>
-
-    </div>
+        </div>
+        <p className={styles.card_price}>{transformPrice(price.unit_amount)} <span>({calculatePriceWIthoutTax(price.unit_amount, product.metadata.taxas)} + {transformPrice(product.metadata.taxas)} taxas)</span></p>
+        <div className={styles.btn_container}>
+          <Button
+            type="button"
+            customStyle="small"
+            onClick={() => addToCart(data)}
+          ><BsFillCartPlusFill color="#000" size="1rem"/></Button>
+          <Button
+            type="button"
+            onClick={() => handleClick(data.price.id)}
+          >Comprar</Button>
+        </div>
+      </>
+    </Card>
   );
 }
