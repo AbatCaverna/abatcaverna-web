58c5d6aac9279e054b7b0192f69a2e844292cc36
Vinicius <vinicius@pop-os.localdomain>
2024-01-21 08:15:32 -0300

    refactor historia and image slide show

--- src/components/Home/Historia/index.tsx
+++ src/components/Home/Historia/index.tsx
@@ -1,5 +1,4 @@
-import ImageSlideView from '../ImageSlideView';
-import styles from './styles.module.css'
+import { ImageSlideView } from 'components/Home'
 
 type Image = {
   img: string;
@@ -11,17 +10,16 @@ interface HistoriaProps {
 }
 
 export default function Historia({ images }: HistoriaProps) {
-  
 
   return (
-    <section id="historia" className={styles.historia}>
-      <h2>História</h2>
-      <div>
-
-        <p>
-          Somos uma república de Florestal MG, idealizada em 2016 e fundada em 2017. 
+    <section id="historia" className="prose w-full max-w-full">
+      <h2 className="text-yellow-500">História</h2>
+      <div className="flex items-center justify-between text-white">
+        <p className="max-w-lg">
+          Somos uma república de Florestal MG, idealizada em 2016 e fundada em 2017.
           Fomos criados não apenas para ser uma república mas uma família, amigos e irmãos.
         </p>
+
         {images && (
           <ImageSlideView
             images={images}
---
+++ src/components/Home/ImageSlideView/ImageSlideView.tsx
@@ -0,0 +1,66 @@
+import Image from 'next/image';
+import Autoplay from 'embla-carousel-autoplay';
+import { useEffect, useRef, useState } from 'react';
+import { Card, CardContent } from '@/components/ui/card';
+import { Carousel, CarouselContent, CarouselItem } from '@/components/ui/carousel';
+
+type Image = {
+  img: string;
+  imgBase64: string;
+}
+
+interface ImageView {
+  images: Array<Image>
+}
+
+export default function ImageView({ images }: ImageView) {
+  const [indexImageOn, setIndexImageOn] = useState(0)
+  const displayImageTime = 5000; // 5 segunds
+  const plugin = useRef(
+    Autoplay({ delay: 2000, stopOnInteraction: true })
+  )
+  function returnImageName(image: string) {
+    // considerando que a imagem vem da pasta public
+    const name = image.split('/')[0];
+
+    return name
+  }
+
+  useEffect(() => {
+
+    setTimeout(() => {
+      if (indexImageOn < images.length - 1) {
+        setIndexImageOn(prev => prev += 1)
+      } else {
+        setIndexImageOn(0)
+      }
+    }, displayImageTime)
+  }, [indexImageOn, images])
+
+  return (
+    <Carousel
+      plugins={[plugin.current]}
+      className="w-full max-w-[600px]"
+      onMouseEnter={plugin.current.stop}
+      onMouseLeave={plugin.current.reset}
+    >
+      <CarouselContent>
+        {images &&
+          images.map((image) => (
+            <CarouselItem key={image.img}>
+              <Card className="bg-transparent border-none">
+                <CardContent className="flex aspect-square items-center justify-center min-h-[400px] min-w-[600px]">
+                  <img
+                    src={image.img}
+                    alt={returnImageName(image.img)}
+                    className="w-full h-full object-cover"
+                  />
+                </CardContent>
+              </Card>
+            </CarouselItem>
+          ))}
+      </CarouselContent>
+    </Carousel>
+  )
+
+}
