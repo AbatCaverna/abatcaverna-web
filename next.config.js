85fc5b2682c1318ed763bd22e0150c7fe5e17e22
Vinicius <vinicius@pop-os.localdomain>
2024-01-25 12:28:08 -0300

    fix build 2

--- next.config.js
+++ next.config.js
@@ -1,16 +1,8 @@
-const withPWA = require('next-pwa')
+const withPWA = require('next-pwa')({
+  dest: 'public',
+})
 
 module.exports = withPWA({
-  webpackDevMiddleware: config => {
-    config.watchOptions = {
-      poll: 1000,
-      aggregateTimeout: 300,
-    }
-    return config
-  },
-  pwa: {
-    dest: "public",
-  },
   images: {
     domains: ["maps.googleapis.com", "lh3.googleusercontent.com", "files.stripe.com"],
   },
