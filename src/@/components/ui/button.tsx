99bbff5ef25266d425ec2bd215d2449c9af296dd
Vinicius <vinicius@pop-os.localdomain>
2024-03-13 21:26:53 -0300

    feat add loading icon and change colors

--- src/@/components/ui/button.tsx
+++ src/@/components/ui/button.tsx
@@ -4,17 +4,19 @@ import { cva } from "class-variance-authority"
 import type { VariantProps } from "class-variance-authority"
 
 import { cn } from "@/lib/utils"
+import { RiLoader2Line, RiLoaderLine } from "react-icons/ri"
+import { AiOutlineLoading } from "react-icons/ai"
 
 const buttonVariants = cva(
   "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        default: "bg-primary text-gray-900 hover:bg-primary/90",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
-          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+          "border text-gray-100 border-input bg-background hover:bg-accent hover:text-accent-foreground",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
@@ -34,21 +36,28 @@ const buttonVariants = cva(
   }
 )
 
+const iconColors: any = {
+  default: '#030712'
+}
+
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
   VariantProps<typeof buttonVariants> {
   asChild?: boolean
+  loading?: boolean
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
+  ({ className, variant, size, asChild = false, loading = false, children, ...props }, ref) => {
     const Comp = asChild ? Slot : "button"
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
         {...props}
-      />
+      >
+        {loading && <AiOutlineLoading className="animate-spin mr-2 text-gray-900" color={variant && iconColors[variant] ? iconColors[variant] : iconColors.default} />} {children}
+      </Comp>
     )
   }
 )
